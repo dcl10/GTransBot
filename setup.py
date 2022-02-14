992e26d1c50270fc52b93f7fc3cbf2022e8edd3f
dcl10 <dan.carr.lea@gmail.com>
2022-02-14 18:12:06 +0000

    add `python-dotenv`

--- setup.py
+++ setup.py
@@ -7,7 +7,14 @@ setup(
     install_requires=[
         "google-cloud-translate==2.0.1",
         "discord.py==1.7.3",
+        "python-dotenv==0.19.2",
     ],
-    extras_require={"dev": ["black==22.1.0", "flake8==4.0.1", "build==0.7.0"]},
+    extras_require={
+        "dev": [
+            "black==22.1.0",
+            "flake8==4.0.1",
+            "build==0.7.0",
+        ]
+    },
     test_suite="tests",
 )
