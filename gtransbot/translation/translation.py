deac2ba2cd60fc410fa0e8f307208b745c05d89b
dcl10 <dan.carr.lea@gmail.com>
2022-02-14 17:40:54 +0000

    draft function to translate text

--- gtransbot/translation/translation.py
+++ gtransbot/translation/translation.py
@@ -8,10 +8,10 @@ def detect_lang(content: str) -> str:
     """Detect the language of a text string.
 
     Args:
-        content (str): The text in unknown language
+        content (str): The text in unknown language.
 
     Returns:
-        str: the detected language
+        str: the detected language.
     """
     languages = client.detect_language(
         parent="projects/translation-discord-bot-341022", content=content
@@ -19,3 +19,24 @@ def detect_lang(content: str) -> str:
     language = languages.languages[0]
     code = language.language_code
     return code
+
+
+def translate_text(content: str, target_lang: str = "ja") -> str:
+    """Translate the content of a string into the target language.
+
+    Args:
+        content (str): The text to be translated.
+        target_lang (str, optional): The target language code. Defaults to "ja".
+
+    Returns:
+        str: The translated content.
+    """
+    response = client.translate_text(
+        contents=[content],
+        target_language_code=target_lang,
+        parent="projects/translation-discord-bot-341022",
+        mime_type="text/plain",
+    )
+    translation = response.translations[0]
+    translated_text = translation.translated_text
+    return translated_text
