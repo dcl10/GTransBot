605697ded0cbeec91a9f3c6e3792292c9dea261a
dcl10 <dan.carr.lea@gmail.com>
2022-02-16 21:28:19 +0000

    add `__init__` to `GTransClient`

--- gtransbot/translation_bot/bot_client.py
+++ gtransbot/translation_bot/bot_client.py
@@ -4,6 +4,17 @@ from gtransbot.translation.translation import detect_lang, translate_text
 
 
 class GTransClient(commands.Bot):
+    def __init__(
+        self,
+        command_prefix,
+        translation_client,
+        help_command=None,
+        description=None,
+        **options
+    ):
+        super().__init__(command_prefix, help_command, description, **options)
+        self.translation_client = translation_client
+
     async def on_message(self, message):
         # Do not reply to bots
         if message.author.bot:
