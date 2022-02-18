3953caecf2702f32a10de2de8ffa5e93942636eb
dcl10 <dan.carr.lea@gmail.com>
2022-02-18 20:53:12 +0000

    bot now processes commands

--- gtransbot/main.py
+++ gtransbot/main.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from google.cloud.translate_v3 import TranslationServiceClient
 from translation_bot.bot_client import GTransClient
+from translation_bot.translation_cog import Translation
 
 load_dotenv(".env")
 
@@ -10,4 +11,5 @@ if __name__ == "__main__":
     bot = GTransClient(
         command_prefix="!", translation_client=TranslationServiceClient()
     )
+    bot.add_cog(Translation())
     bot.run(os.getenv("DISCORD_BOT_TOKEN"))
